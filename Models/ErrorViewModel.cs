a354d0496ad9a244b7ecf14a55c63d5c4d6e42d9
ART-G-55 <egomezdi@uninpahu.edu.co>
2025-05-21 14:51:02 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trayectos_Especiales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
